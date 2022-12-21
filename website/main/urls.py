e78b195796e6ef52ea1ab8b8179245a522b5ff10
Atanas Stankin <a_stankin@abv.bg>
2022-12-21 08:39:53 +0200

    Adding Sign up form

--- website/main/urls.py
+++ website/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    path('sign_up', views.sign_up, name='sign_up'),
 ]
