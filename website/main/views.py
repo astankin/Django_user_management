8b329972c703d99ba61e972b6ebb36a7e1158228
Atanas Stankin <a_stankin@abv.bg>
2022-12-23 10:56:43 +0200

    Added Post Form

--- website/main/views.py
+++ website/main/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, PostForm
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import login, logout, authenticate
 
+
 # Create your views here.
+
+@login_required(login_url='/login')
 def home(request):
     return render(request, 'main/home.html')
+
+
+@login_required(login_url="/login")
+#@permission_required("main.add_post", login_url="/login", raise_exception=True)
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("/home")
+    else:
+        form = PostForm()
+
+    return render(request, 'main/create_post.html', {"form": form})
 
 
 def sign_up(request):
