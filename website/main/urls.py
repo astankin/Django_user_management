c17096d8e0945e037696f09b5727d6d3c726eb01
Atanas Stankin <a_stankin@abv.bg>
2022-12-27 10:46:03 +0200

    Fixed bug with database column

--- website/main/urls.py
+++ website/main/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
-    path('sign-up', views.sign_up, name='sign_up'),
+    path('sign_up', views.sign_up, name='sign_up'),
     path('create-post', views.create_post, name='create_post'),
 ]
